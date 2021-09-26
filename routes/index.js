106814af5fefacfed44e49d2723fe921554ef1ba
Emily81926 <emily81926@gmail.com>
2021-09-26 18:41:44 +0800

    feat: Add isAdmin to users table, Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,28 @@ const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
 
-  app.get('/', (req, res) => res.redirect('/restaurants'))
+  const authenticated = (req, res, next) => {
+    if(req.isAuthenticated()){
+      return next()
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/restaurants',restController.getRestaurants)
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if(req.user.isAdmin){ return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/', authenticated,(req, res) => res.redirect('/restaurants'))
 
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/restaurants', authenticated,restController.getRestaurants)
+
+  app.get('/admin', authenticatedAdmin,(req, res) => res.redirect('/admin/restaurants'))
+
+  app.get('/admin/restaurants', authenticatedAdmin,adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
 
